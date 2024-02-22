954fc678487d8c7690f0b9699ce42839649aabc3
yerramreddyuday <uday.prasad@dojima.network>
2024-02-22 14:55:49 +0530

    dev: mod: Update hardhat config file

--- backend/hardhat.config.ts
+++ backend/hardhat.config.ts
@@ -19,8 +19,7 @@ module.exports = {
   },
   networks: {
     dojimachain: {
-      url: "http://localhost:8545",
-      // url: 'https://api-test.d11k.dojima.network',
+      url: 'https://api-dev.d11k.dojima.network',
       // url: process.env.VITE_APP_DOJIMA_API_URL,
       chainId: 1001,
       // gas: 5000000, //units of gas you are willing to pay, aka gas limit
@@ -28,13 +27,10 @@ module.exports = {
       accounts: [
         // process.env.VITE_APP_TEST_ACCOUNT_PVTKEY,
         "ae1d058b9c5713848e7ac4c1901fac9a737729a34c10c997991c861dd7705ac0",
-        // "OUTBOUND_STATE_SENDER_CONTRACT_ADDRESS": "0x5D62B90c5D5061503c068bE0A39a38764A965fdC",
-        // "STATE_SYNCER_VERIFIER_CONTRACT_ADDRESS": "0xb1D6d50D93547a324C2740E424A694D1BE51aCfa"
       ],
     },
     goerli: {
-      url: "http://localhost:9545",
-      // url: `https://eth-goerli.g.alchemy.com/v2/TIMeEU-fdUdyD-YijUoB_AbdtlVfEcl2`,
+      url: `https://eth-goerli.g.alchemy.com/v2/TIMeEU-fdUdyD-YijUoB_AbdtlVfEcl2`,
       // url: process.env.VITE_APP_ETH_GOERLI_ALCHEMY_URL,
       chainId: 1337,
       // gas: 5000000, //units of gas you are willing to pay, aka gas limit
@@ -50,12 +46,20 @@ module.exports = {
       gas: 5000000, // units of gas you are willing to pay, aka gas limit
       gasPrice: 225000000000, // gas is typically in units of gwei, but you must enter it as wei here
     },
+    bsc: {
+      url: "https://data-seed-prebsc-1-s1.bnbchain.org:8545",
+      chainId: 97,
+      gasPrice: 20000000000,
+      accounts: [
+        "ae1d058b9c5713848e7ac4c1901fac9a737729a34c10c997991c861dd7705ac0",
+      ],
+    },
   },
   etherscan: {
     apiKey: {
-      goerli: process.env.VITE_APP_ETH_ETHERSCAN_APIKEY,
-      // goerli: "6IU4JG5P2PNVRSB54YIAMIAQFQ879PXJ7C",
+      ethereum: "6IU4JG5P2PNVRSB54YIAMIAQFQ879PXJ7C",
       dojimachain: 'dojima',
+      bsc: "7X1FZ7XNHKPHMXA6VXFX56I533BP7KW3MW",
     },
     customChains: [
       {
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,8 +1,9 @@
 /** @type import('hardhat/config').HardhatUserConfig */
-require("@nomicfoundation/hardhat-toolbox");
-require("@nomicfoundation/hardhat-ethers");
+require('@nomicfoundation/hardhat-toolbox');
+require('@nomicfoundation/hardhat-ethers');
+require('@openzeppelin/hardhat-upgrades');
 // require("@nomiclabs/hardhat-truffle5");
-require("@nomicfoundation/hardhat-chai-matchers");
+require("@nomicfoundation/hardhat-chai-matchers")
 // require('hardhat-ethernal');
 // require("hardhat-tracer");
 
@@ -12,26 +13,32 @@ module.exports = {
     settings: {
       optimizer: {
         enabled: true,
-        runs: 200,
-      },
-    },
+        runs: 200
+      }
+    }
   },
   networks: {
     dojimachain: {
-      // url: "http://localhost:8545",
-      url: process.env.VITE_APP_DOJIMA_API_URL,
+      url: 'https://api-dev.d11k.dojima.network',
+      // url: process.env.VITE_APP_DOJIMA_API_URL,
       chainId: 1001,
       // gas: 5000000, //units of gas you are willing to pay, aka gas limit
       gasPrice: 2000000000, // gas is typically in units of gwei, but you must enter it as wei here
-      accounts: [process.env.VITE_APP_TEST_ACCOUNT_PVTKEY],
+      accounts: [
+        // process.env.VITE_APP_TEST_ACCOUNT_PVTKEY,
+        "ae1d058b9c5713848e7ac4c1901fac9a737729a34c10c997991c861dd7705ac0",
+      ],
     },
     goerli: {
-      // url: "http://localhost:9545",
-      url: process.env.VITE_APP_ETH_GOERLI_ALCHEMY_URL,
-      // chainId: 1337,
+      url: `https://eth-goerli.g.alchemy.com/v2/TIMeEU-fdUdyD-YijUoB_AbdtlVfEcl2`,
+      // url: process.env.VITE_APP_ETH_GOERLI_ALCHEMY_URL,
+      chainId: 1337,
       // gas: 5000000, //units of gas you are willing to pay, aka gas limit
       gasPrice: 2000000000, // gas is typically in units of gwei, but you must enter it as wei here
-      accounts: [process.env.VITE_APP_TEST_ACCOUNT_PVTKEY],
+      accounts: [
+        // process.env.VITE_APP_TEST_ACCOUNT_PVTKEY,
+        "ae1d058b9c5713848e7ac4c1901fac9a737729a34c10c997991c861dd7705ac0",
+      ],
     },
     avalanche: {
       url: "http://127.0.0.1:9650/ext/bc/C/rpc",
@@ -39,20 +46,33 @@ module.exports = {
       gas: 5000000, // units of gas you are willing to pay, aka gas limit
       gasPrice: 225000000000, // gas is typically in units of gwei, but you must enter it as wei here
     },
+    bsc: {
+      url: "https://data-seed-prebsc-1-s1.bnbchain.org:8545",
+      chainId: 97,
+      gasPrice: 20000000000,
+      accounts: [
+        "ae1d058b9c5713848e7ac4c1901fac9a737729a34c10c997991c861dd7705ac0",
+      ],
+    },
   },
   etherscan: {
     apiKey: {
-      goerli: process.env.VITE_APP_ETH_ETHERSCAN_APIKEY,
-      dojimachain: "dojima",
+      ethereum: "6IU4JG5P2PNVRSB54YIAMIAQFQ879PXJ7C",
+      dojimachain: 'dojima',
+      bsc: "7X1FZ7XNHKPHMXA6VXFX56I533BP7KW3MW",
     },
     customChains: [
       {
-        network: "dojimachain",
+        network: 'dojimachain',
         chainId: 1001,
         urls: {
           apiURL: process.env.VITE_APP_DOJIMA_BLOCKSCOUT_URL,
-          browserURL: process.env.VITE_APP_DOJIMA_BLOCKSCOUT_API_URL,
+          browserURL: process.env.VITE_APP_DOJIMA_BLOCKSCOUT_API_URL
         },
+        // urls: {
+        //   apiURL: 'https://doj-bex-test.dojima.network/api',
+        //   browserURL: 'https://doj-bex-test.dojima.network/',
+        // },
       },
     ],
   },
